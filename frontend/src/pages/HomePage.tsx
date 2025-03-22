c7cfea92778b670c6cfeab023bb169e36eb82e69
berkay-g <berkay-g@users.noreply.github.com>
2025-03-22 17:34:22 +0300

    small fix

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -10,11 +10,14 @@ const HomePage = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true); // State to toggle sidebar
 
     return (
-        <div className="h-screen bg-base-200">
+        <div className="min-h-[100dvh] bg-base-200">
             <div className="flex items-center justify-center pt-20 px-4">
                 <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
                     <div className="flex h-full rounded-lg overflow-hidden">
-                        <SideBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
+                        <SideBar
+                            isSidebarOpen={isSidebarOpen}
+                            setIsSidebarOpen={setIsSidebarOpen}
+                        />
 
                         {!selectedUser ? (
                             <NoChatSelected
