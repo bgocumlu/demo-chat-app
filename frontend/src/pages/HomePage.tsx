6580d29c0a9abfd1312bcfaab1f39fb552e0218a
berkay-g <berkay-g@users.noreply.github.com>
2025-03-22 17:40:54 +0300

    mobile fix

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,16 +3,18 @@ import NoChatSelected from "@/components/NoChatSelected";
 import { SideBar } from "@/components/SideBar";
 import { useChatStore } from "@/store/useChatStore";
 import { useState } from "react";
-// import {ContactsDrawer} from "@/components/ContactsDrawer";
 
 const HomePage = () => {
     const { selectedUser } = useChatStore();
     const [isSidebarOpen, setIsSidebarOpen] = useState(true); // State to toggle sidebar
 
     return (
-        <div className="min-h-[100dvh] bg-base-200">
+        <div
+            className="min-h-[100dvh] bg-base-200"
+            style={{ paddingBottom: "env(safe-area-inset-bottom)" }} // Add padding for the safe area
+        >
             <div className="flex items-center justify-center pt-20 px-4">
-                <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+                <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100dvh-8rem)]">
                     <div className="flex h-full rounded-lg overflow-hidden">
                         <SideBar
                             isSidebarOpen={isSidebarOpen}
